f2ece1dfe1ad48b146ec9054bfd00c01b3d8f243
Bimmka <bibimmka@gmail.com>
2024-09-17 11:37:33 +0200

    Change matcher

--- source/gmtk/Assets/Code/Gameplay/Features/Rabbits/SubFeatures/Replication/Systems/StopGoToInvalidReplicationTargetSystem.cs
+++ source/gmtk/Assets/Code/Gameplay/Features/Rabbits/SubFeatures/Replication/Systems/StopGoToInvalidReplicationTargetSystem.cs
@@ -17,7 +17,8 @@ namespace Code.Gameplay.Features.Rabbits.SubFeatures.Replication.Systems
                     GameMatcher.ReplicationTarget,
                     GameMatcher.WantToReplicate,
                     GameMatcher.RabbitVisualChanger,
-                    GameMatcher.MoveDirection));
+                    GameMatcher.MoveDirection,
+                    GameMatcher.MovingToReplicationTarget));
         }
 
         public void Execute()
