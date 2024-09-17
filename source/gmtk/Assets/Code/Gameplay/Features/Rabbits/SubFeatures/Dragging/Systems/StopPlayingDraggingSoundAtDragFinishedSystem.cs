aa209922af12e96b4724b8fc47a304d94136617c
Bimmka <bibimmka@gmail.com>
2024-09-17 11:39:07 +0200

    Missed buffer

--- source/gmtk/Assets/Code/Gameplay/Features/Rabbits/SubFeatures/Dragging/Systems/StopPlayingDraggingSoundAtDragFinishedSystem.cs
+++ source/gmtk/Assets/Code/Gameplay/Features/Rabbits/SubFeatures/Dragging/Systems/StopPlayingDraggingSoundAtDragFinishedSystem.cs
@@ -1,5 +1,4 @@
-﻿using Code.Gameplay.Sounds.Config;
-using Code.Gameplay.Sounds.Service;
+﻿using System.Collections.Generic;
 using Entitas;
 
 namespace Code.Gameplay.Features.Rabbits.SubFeatures.Dragging.Systems
@@ -7,6 +6,7 @@ namespace Code.Gameplay.Features.Rabbits.SubFeatures.Dragging.Systems
     public class StopPlayingDraggingSoundAtDragFinishedSystem : IExecuteSystem
     {
         private readonly IGroup<GameEntity> _rabbits;
+        private readonly List<GameEntity> _buffer = new List<GameEntity>(8);
 
         public StopPlayingDraggingSoundAtDragFinishedSystem(GameContext game)
         {
@@ -19,7 +19,7 @@ namespace Code.Gameplay.Features.Rabbits.SubFeatures.Dragging.Systems
 
         public void Execute()
         {
-            foreach (GameEntity rabbit in _rabbits)
+            foreach (GameEntity rabbit in _rabbits.GetEntities(_buffer))
             {
                 rabbit.HuntSoundElement?.Reset();
                 rabbit.RemoveHuntSoundElement();
